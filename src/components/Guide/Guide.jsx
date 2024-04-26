861b7783c8ed5e72a5b1b123aad729884b756cfa
Mayank Gupta <gupta.mayank2003@gmail.com>
2024-04-26 17:29:06 +0530

    updated guide

--- src/components/Guide/Guide.jsx
+++ src/components/Guide/Guide.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./Guide.css";
 import { SignImageData } from "../../data/SignImageData";
 import { useSelector } from "react-redux";
+import all_images from "../../assests/all_images.png";
 // import Footer from "../footer/Footer";
 
 const Guide = () => {
@@ -24,7 +25,7 @@ const Guide = () => {
         <div className="signlang_imagelist-container">
           <div className="signlang_image-div">
             {currentImage ? (
-              <img src={currentImage.url} alt={currentImage.name} />
+              <img src={all_images} alt="all signs" className="all_image" />
             ) : (
               <h3 className="gradient__text">
                 Please wait while we are generating the images for you...
